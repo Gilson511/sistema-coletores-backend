0db7162dae13e2e870a709f8d9c85303670124c7
Gilson511 <araujogilson831@gmail.com>
2025-07-13 14:14:14 -0300

    u

--- database.js
+++ database.js
@@ -1,11 +1,13 @@
+require("dotenv").config(); // para ler as variáveis do .env
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "coletores_db",
-  password: "2525",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 module.exports = pool;

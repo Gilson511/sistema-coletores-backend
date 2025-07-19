880daa9fdec99682d40a90c06be5831aad1260ba
Gilson511 <araujogilson831@gmail.com>
2025-07-18 22:29:40 -0300

    teste

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-// Middlewares globais
-app.use(cors());
+const PORT = process.env.PORT || 3000;
+
+app.use(cors()); // Ou configure o origin aqui se quiser restringir
+
 app.use(express.json());
 
-// Rotas
 const coletoresRoutes = require("./routes/coletores");
 app.use("/api/coletores", coletoresRoutes);
 
@@ -19,7 +20,10 @@ app.use("/api/basecoletores", baseColetoresRoutes);
 const colaboradoresRoutes = require("./routes/colaboradores");
 app.use("/api/colaboradores", colaboradoresRoutes);
 
-// Inicia o servidor
-app.listen(3000, '0.0.0.0', () => {
-  console.log("✅ Servidor rodando em http://0.0.0.0:3000");
+app.get("/", (req, res) => {
+  res.send("✅ API está online e funcionando!");
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Servidor rodando em http://0.0.0.0:${PORT}`);
 });

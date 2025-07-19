9588b177685037b1c751ab032b28055e56e4bacd
Gilson511 <araujogilson831@gmail.com>
2025-07-18 21:01:14 -0300

    mais uma atualização

--- database.js
+++ database.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "coletores_db",
-  password: "2525",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
